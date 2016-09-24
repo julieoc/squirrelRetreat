f98348631c03062988b456bc7b1eee8739776a32
Julie <julieoanhcao@MacBook-Pro.local>
2016-09-24 18:48:10 +1000

    admin.php: Setup to disply as tables and turn on all display types

--- admin.php
+++ admin.php
@@ -5,7 +5,7 @@
 <body>
 
 <?php
-
+require 'generalFunctions.php';
 require 'dbConnect.php';
 require 'pollResults.php';
 //require 'bookingInformation.php';
@@ -14,28 +14,77 @@ require 'pollResults.php';
 $conn = dbConnect();
 
 $pollResults = getPollResults($conn);
-//$bookings = getBookingHistory($conn);
-//$customers = getCustomerInformation($conn);
-
-foreach ($pollResults as $row) {
-  echo "{$row['QNUM']} - {$row['QUESTION']}:    {$row['AVG_RATING']} ({$row['NUMVOTES']} votes cast)<br>";
-}
-
-/*
-foreach ($bookings as $row) {
-  echo "BOOKING!!<br>";
-}
-/**/
-
-/*
-foreach ($customers as $row) {
-  echo "CUSTOMER!!<br>";
-}
-/**/
-
+$bookings = getBookingHistory($conn);
+$customers = getCustomerInformation($conn);
 dbClose($conn);
 
 ?>
+<table>
+<th><td> POLL RESULTS </td></th>
+<?php
+foreach ($pollResults as $row) {
+?>
+<tr>
+<td><?php echoSafeText($row['QNUM']); ?></td>
+<td><?php echoSafeText($row['QUESTION']); ?></td>
+<td><?php echoSafeText($row['AVG_RATING']); ?></td>
+<td><?php echoSafeText($row['NUMVOTES']); ?></td>
+</tr>
+<?php 
+} 
+?>
+</table>
+<table>
+<th><td> BOOKING INFORMATION</td></th>
+<?php
+foreach($bookings as $row){
+?>
+<tr>
+<td><?php echoSafeText($row['bookID']); ?></td>
+<td><?php echoSafeText($row['roomNumber']); ?></td>
+<td><?php echoSafeText($row['customerID']); ?></td>
+<td><?php echoSafeText($row['checkIn']); ?></td>
+<td><?php echoSafeText($row['checkOut']); ?></td>
+<td><?php echoSafeText($row['adults']); ?></td>
+<td><?php echoSafeText($row['children']); ?></td>
+</tr>
+}
+?>
+</table>
+<table>
+<th><td> CUSTOMER INFORMATION </td></th>
+<?php
+foreach($customers as $row){
+?>
+<tr>
+<td><?php echoSafeText($row['customerID']); ?></td>
+<td><?php echoSafeText($row['title']);?></td>
+<td><?php echoSafeText($row['firstName']);?></td>
+<td><?php echoSafeText($row['lastName']);?></td>
+<td><?php echoSafeText($row['middleName']);?></td>
+<td><?php echoSafeText($row['email']);?></td>
+<td><?php echoSafeText($row['homePh']);?></td>
+<td><?php echoSafeText($row['mobilePh']);?></td>
+<td><?php echoSafeText($row['address']);?></td>
+<td><?php echoSafeText($row['city']);?></td>
+<td><?php echoSafeText($row['postcode']);?></td>
+<td><?php echoSafeText($row['state']);?></td>
+<td><?php echoSafeText($row['country']);?></td>
+<td><?php echoSafeText($row['cardType']);?></td>
+<td><?php echoSafeText($row['cardName']);?></td>
+<td><?php echoSafeText($row['cardNumber']);?></td>
+<td><?php echoSafeText($row['expMonth']);?></td>
+<td><?php echoSafeText($row['expYear']);?></td>
+<td><?php echoSafeText($row['verificationCd']);?></td>
+<td><?php echoSafeText($row['billAddress']);?></td>
+<td><?php echoSafeText($row['billCity']);?></td>
+<td><?php echoSafeText($row['billPostcode']);?></td>
+<td><?php echoSafeText($row['billState']);?></td>
+<td><?php echoSafeText($row['billCountry']);?></td>
+}
+?>
+</table>
+
 
 </body>
 </html>
