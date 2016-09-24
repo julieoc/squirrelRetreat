12f75140a14a3270a8ebfe46fff2f2c7ae7988ce
Tom Coleman <igenous@gmail.com>
2016-09-25 08:59:49 +1000

    display of data from database ordered

--- bookingInformation.php
+++ bookingInformation.php
@@ -2,7 +2,7 @@
 session_start();
 // bookingInformation.php
 function getBookingHistory($dbh) {
-  $qry = 'SELECT * FROM BOOKING';
+  $qry = 'SELECT * FROM BOOKING ORDER BY checkIn, checkOut, roomNumber, bookID';
   $oci_qry = oci_parse($dbh, $qry);
   oci_execute($oci_qry);
   $nrows = oci_fetch_all($oci_qry, $data, null, null, OCI_FETCHSTATEMENT_BY_ROW);
--- customerData.php
+++ customerData.php
@@ -2,7 +2,7 @@
 session_start();
 // customerData.php
 function getCustomerInformation($dbh) {
-  $qry = 'SELECT * FROM CUSTOMER';
+  $qry = 'SELECT * FROM CUSTOMER ORDER BY customerID';
   $oci_qry = oci_parse($dbh, $qry);
   oci_execute($oci_qry);
   $nrows = oci_fetch_all($oci_qry, $data, null, null, OCI_FETCHSTATEMENT_BY_ROW);
