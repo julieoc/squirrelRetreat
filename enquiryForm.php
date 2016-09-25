3b6b57991de91d9fece542845c321d053512631f
Julie <julieoanhcao@MacBook-Pro.local>
2016-09-25 12:11:17 +1000

    enquiryForm.php: Order query results and fix incorrect varibale

--- enquiryForm.php
+++ enquiryForm.php
@@ -1,9 +1,10 @@
 <?php
-// customerEnquiry.php
+session_start();
+// enquiryForm.php
 function getCustomerEnquiry($dbh) {
-  $qry = 'SELECT * FROM ENQUIRYFORM';
+  $qry = 'SELECT * FROM ENQUIRYFORM ORDER BY enquiryNo';
   $oci_qry = oci_parse($dbh, $qry);
-  oci_execute($ocy_qry);
+  oci_execute($oci_qry);
   $nrows = oci_fetch_all($oci_qry, $data, null, null, OCI_FETCHSTATEMENT_BY_ROW);
 
   return $data;
